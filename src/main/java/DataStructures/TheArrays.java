df42b8a2f829308ba0627c4797339e99941bc6f3
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-10 14:44:36 +0300

    Arrays in java

---
+++ src/main/java/DataStructures/TheArrays.java
@@ -0,0 +1,15 @@
+package DataStructures;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String [] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(colors);
+        System.out.println(Arrays.toString(colors));
+        System.out.println(colors[1]);
+    }
+}
