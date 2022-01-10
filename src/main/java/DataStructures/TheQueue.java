7e5b5c3f710a8e620f0f7a6c22b6043625712e87
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-10 16:36:06 +0300

    linked list queue implementation in java

---
+++ src/main/java/DataStructures/TheQueue.java
@@ -0,0 +1,28 @@
+package DataStructures;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//FIFO (First In First Out
+//A collection designed for holding elements prior to processing
+public class TheQueue {
+    public static void main(String[] args) {
+        Queue<Person> supermarket = new LinkedList<>();
+
+        supermarket.add(new Person("Alex", 23));
+        supermarket.add(new Person("Ben", 24));
+        supermarket.add(new Person("Charlie", 25));
+        supermarket.add(new Person("David", 26));
+
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+
+    }
+
+    static record Person(String name, int age){
+
+    }
+}
