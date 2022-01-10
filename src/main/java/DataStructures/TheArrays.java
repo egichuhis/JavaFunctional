9e7fc2ac9583d34f5fd7131ad936fd5f8464748e
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-10 14:55:41 +0300

    Arrays in java 1D

--- src/main/java/DataStructures/TheArrays.java
+++ src/main/java/DataStructures/TheArrays.java
@@ -11,5 +11,17 @@ public class TheArrays {
         System.out.println(colors);
         System.out.println(Arrays.toString(colors));
         System.out.println(colors[1]);
+
+        int [] numbers = {12, 13, 14, 15, 16};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+        for (int number: numbers ) {
+            System.out.println(number);
+        }
+        Arrays.stream(colors).forEach(System.out::println);
     }
 }
