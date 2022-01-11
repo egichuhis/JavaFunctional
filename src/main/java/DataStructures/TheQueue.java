9930e462c52e5bd9febadb20fd21822101ef320c
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-11 16:27:22 +0300

    linkedlist

--- src/main/java/DataStructures/TheQueue.java
+++ src/main/java/DataStructures/TheQueue.java
@@ -1,6 +1,7 @@
 package DataStructures;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 //FIFO (First In First Out
@@ -8,15 +9,28 @@ import java.util.Queue;
 public class TheQueue {
     public static void main(String[] args) {
 LinkedList<Person> linkedList = new LinkedList<>();
+
+linkedList.add(new Person("Alex", 23));
+linkedList.add(new Person("Ben", 24));
+linkedList.add(new Person("Charlie", 25));
+
+ListIterator<Person> personListIterator = linkedList.listIterator();
+while (personListIterator.hasNext()){
+    System.out.println(personListIterator.next());
+}
+        System.out.println();
+        while (personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
     }
 
     private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
 
-        supermarket.add(new Person("Alex", 23));
-        supermarket.add(new Person("Ben", 24));
-        supermarket.add(new Person("Charlie", 25));
-        supermarket.add(new Person("David", 26));
+//        supermarket.add(new Person("Alex", 23));
+//        supermarket.add(new Person("Ben", 24));
+//        supermarket.add(new Person("Charlie", 25));
+//        supermarket.add(new Person("David", 26));
 
         System.out.println(supermarket.size());
         System.out.println(supermarket.peek());
