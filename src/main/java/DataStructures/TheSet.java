b7a1856d5b5c81bbd8b0004f0a0c1947bfb7888e
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-11 16:35:17 +0300

    hash set

---
+++ src/main/java/DataStructures/TheSet.java
@@ -0,0 +1,23 @@
+package DataStructures;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+//A collection that contains no duplicate elements, no pair of elements
+//unordered elements
+public class TheSet {
+    public static void main(String[] args) {
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("red"));
+        balls.add(new Ball("yellow"));
+
+
+        balls.forEach(System.out::println);
+    }
+
+    record Ball(String color){
+
+    }
+}
