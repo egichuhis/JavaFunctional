f53af2dbc2ddcb7a6f3b3ba8fbba37ca12388048
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-11 17:21:42 +0300

    map methods

--- src/main/java/DataStructures/TheMaps.java
+++ src/main/java/DataStructures/TheMaps.java
@@ -1,4 +1,37 @@
 package DataStructures;
 
+
+//maps --> a collection of key-value pairs
+//HashTable-->Null not allowed
+//HashMap-->Null allowed
+//SortedMap
+
+//key cannot be duplicate, and you can assign utmost one value to each
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TheMaps {
+    public static void main(String[] args) {
+        Map<Integer, Person> map = new HashMap<>();
+
+        map.put(1, new Person("Alex"));
+        map.put(2, new Person("Arnold"));
+        map.put(3, new Person("Mariam"));
+//        map.put(1, new Person("Edwin"));
+        map.remove(3);
+
+
+        System.out.println(map);
+        System.out.println(map.size());
+        System.out.println(map.get(2));
+        System.out.println(map.containsKey(2));
+        System.out.println(map.keySet());
+        System.out.println(map.entrySet());
+
+        map.entrySet().forEach(System.out::println);
+
+    }
+
+    record Person(String name){}
 }
